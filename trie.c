eeb7fb6ea81ac3c230bcb5e66d5470a8005008d4
okovko <okovko@gmail.com>
2020-10-13 23:57:19 -0700

    Basic trie implemented. Build, search, deallocate. Basic testing.

---
+++ trie.c
@@ -0,0 +1,103 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "trie.h"
+
+struct trie_node *trie_make_node(const char letter) {
+  struct trie_node *const new = malloc(sizeof *new);
+  if (new == NULL) {
+    // error
+  }
+  *new = (struct trie_node) {.letter = letter};
+  return new;
+}
+
+struct trie_node *trie_level_get_tail(const struct trie_node *const level) {
+  assert(level != NULL);
+  const struct trie_node *trav = level;
+  while (trav->next != NULL) {
+    trav = trav->next;
+  }
+  return (struct trie_node *)trav;
+}
+
+void trie_level_append_node(const struct trie_node *const level, const struct trie_node *const node) {
+  struct trie_node *const tail = trie_level_get_tail(level);
+  tail->next = (struct trie_node *)node;
+}
+
+struct trie_node *trie_level_search(const struct trie_node *const level, const char letter) {
+  const struct trie_node *trav = (struct trie_node *)level;
+  while (trav != NULL && trav->letter != letter) {
+    trav = trav->next;
+  }
+  return (struct trie_node *)trav;
+}
+
+struct trie_search_result trie_search(const struct trie *const trie, const char *str) {
+  assert(trie != NULL);
+  assert(str != NULL);
+  const struct trie_node *level = trie->root,
+                         *curr = NULL,
+                         *prev = NULL;
+  for (char c = *str; c != '\0'; str++, c = *str) {
+    prev = curr;
+    curr = trie_level_search(level, c);
+    if (curr != NULL) {
+      level = curr->down;
+    }
+    else {
+      break;
+    }
+  }
+  return (struct trie_search_result){str, (struct trie_node *)prev};
+}
+
+void trie_add_entry(struct trie *const trie, const char *str, void *data) {
+  // TODO: make less ugly, adding a level should be its own function
+  assert(trie != NULL);
+  assert(str != NULL);
+  // assert(data != NULL);
+  struct trie_search_result search = trie_search(trie, str);
+  str = search.rest;
+  struct trie_node *node = search.last;
+  if (*str == '\0') {
+    return;
+  }
+  if (trie->root == NULL) {
+    trie->root = trie_make_node(*str++);
+    node = trie->root;
+  }
+  else if (node == NULL) {
+    trie->root->next = trie_make_node(*str++);
+    node = trie->root->next;
+  }
+  else if (node->down != NULL) {
+    // TODO: smarter level insertion
+    node = trie_level_get_tail(node->down);
+    node->next = trie_make_node(*str++);
+    node = node->next;
+  }
+  for (char c = *str; c != '\0'; str++, c = *str) {
+    node->down = trie_make_node(c);
+    node = node->down;
+  }
+  node->data = data;
+}
+
+void trie_dealloc_nodes(struct trie_node **const node) {
+  // TODO: make / dealloc trie itself, instead of root
+  // curiously, const struct trie_node **const creates a type nightmare
+  // dereferencing ^ gives struct trie_node *const *
+  // the rules for double pointer const results are apparently tricky
+  if ((*node)->down != NULL) {
+    trie_dealloc_nodes(&(*node)->down);
+  }
+  if ((*node)->next != NULL) {
+    trie_dealloc_nodes(&(*node)->next);
+  }
+  free(*node);
+  *node = NULL;
+}
---
+++ trie_tests.c
@@ -0,0 +1,63 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "trie.h"
+
+/*
+  quick hack for debugging
+  won't print any entry that is a subset of another
+  so it prints terminating paths, not entries
+  example: if can and canada are both entries, only canada will print
+ */
+void print_paths(const struct trie_node *const node, int pos) {
+  static char prefix[256];
+  char c = node->letter;
+  if (node->down != NULL) {
+    prefix[pos] = c;
+    printf("%c", c);
+    print_paths(node->down, pos + 1);
+  }
+  else {
+    printf("%c\n", c);
+  }
+  if (node->next != NULL) {
+    for (int i = 0; i < pos; i++) {
+      printf("%c", prefix[i]);
+    }
+    print_paths(node->next, pos);
+  }
+}
+
+/*
+  visit every node
+ */
+void print_nodes(struct trie_node *node) {
+  printf("%c", node->letter);
+  if (node->down != NULL) {
+    print_nodes(node->down);
+  }
+  if (node->next != NULL) {
+    printf("\n");
+    print_nodes(node->next);
+  }
+}
+
+int main() {
+  // TODO: write actual tests
+  struct trie trie = {.root = NULL};
+  trie_add_entry(&trie, "can", NULL);
+  trie_add_entry(&trie, "canada", NULL);
+  trie_add_entry(&trie, "canadian", NULL);
+  trie_add_entry(&trie, "cool", NULL);
+  trie_add_entry(&trie, "cent", NULL);
+  trie_add_entry(&trie, "cell", NULL);
+  trie_add_entry(&trie, "ceiling", NULL);
+  trie_add_entry(&trie, "cellar", NULL);
+  trie_add_entry(&trie, "shell", NULL);
+  trie_add_entry(&trie, "smell", NULL);
+  trie_add_entry(&trie, "shoe", NULL);
+  print_paths(trie.root, 0);
+  trie_dealloc_nodes(&trie.root);
+}
